d56347b7ba38a64e9609676ec3e2093a2378f659
Jummate <yakubjumat@gmail.com>
2022-10-25 23:00:48 +0100

    Welcome image added and made to be repsonsive

--- presentation-layer/src/containers/Welcome/Welcome.js
+++ presentation-layer/src/containers/Welcome/Welcome.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Button from '../../components/button/Button';
-// import {ninja-image} from
+import NinjaImage from '../../assets/ninja-gaming-joystick-sport-logo-icon.jpg';
 import './Welcome.css';
 
 const Welcome = () => {
   return (
     <section className='Welcome__container'>
       <h1 className='Welcome__heading'>Guess Ninja</h1>
-      {/* <img src={}/> */}
+      <img
+        className='Welcome__image'
+        src={NinjaImage}
+        alt='Ninja Gaming Joystick Sport Logo'
+      />
       <Button buttonSize='btn--large btn--gradient'> Proceed &gt;&gt;</Button>
     </section>
   );
