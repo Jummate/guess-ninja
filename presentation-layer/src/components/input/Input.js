49f2c6f052e29917f84aaa16ee86f82be2beeb63
Jummate <yakubjumat@gmail.com>
2022-10-31 23:29:09 +0100

    starts tidying up the GameSetup page

---
+++ presentation-layer/src/components/input/Input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Input.css';
+
+const Input = () => {
+  return (
+    <input
+      type='text'
+      className='input'
+      placeholder='This is an input component'
+    />
+  );
+};
+
+export default Input;
--- presentation-layer/src/containers/GameSetup/GameSetup.js
+++ presentation-layer/src/containers/GameSetup/GameSetup.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './GameSetup.css';
+import Input from '../../components/input/Input';
 
 const GameSetup = () => {
   return (
-    <section>
-      <h1>This is the Game Set up page</h1>
+    <section className='GameSetup__container'>
+      <h1 className='GameSetup__heading'>Game Setup</h1>
+      <h3 className='GameSetup__text'>Specify the gameplan</h3>
+      <Input />
     </section>
   );
 };
