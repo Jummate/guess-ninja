e58ccc0a8f9014eaf94f6747876b8f9be5e0278f
Jummate <yakubjumat@gmail.com>
2022-11-02 00:00:11 +0100

    update the GameSetup page with difficulty selection and submit button

--- presentation-layer/src/containers/GameSetup/GameSetup.js
+++ presentation-layer/src/containers/GameSetup/GameSetup.js
@@ -2,13 +2,35 @@ import React from 'react';
 import './GameSetup.css';
 import Input from '../../components/input/Input';
 import Navigation from '../../components/nav/Navigation';
+import Button from '../../components/button/Button';
 
 const GameSetup = () => {
   return (
     <section className='GameSetup__container'>
       <h1 className='GameSetup__heading'>Game Setup</h1>
       <h3 className='GameSetup__text'>Specify the gameplan</h3>
-      <Input />
+      <form>
+        <label>Number of Players</label>
+        <Input />
+
+        <label className='difficulty-label'>Difficulty</label>
+        <select
+          name='difficult'
+          className='GameSetup__difficulty'
+        >
+          <option value='Easy'>Easy</option>
+          <option value='Medium'>Medium</option>
+          <option value='Hard'>Hard</option>
+        </select>
+
+        <Button
+          buttonSize='btn--large'
+          buttonStyle='btn--gradient'
+        >
+          Submit
+        </Button>
+      </form>
+
       <Navigation />
     </section>
   );
