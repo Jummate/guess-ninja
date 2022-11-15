6a15b66f1c5f7bad0802f8ece0ab0de907426a24
Jummate <yakubjumat@gmail.com>
2022-11-15 23:39:21 +0100

    add and link all the major pages to one another in readiness for tidying up

--- presentation-layer/src/containers/GuessTaking/GuessTaking.js
+++ presentation-layer/src/containers/GuessTaking/GuessTaking.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Input from '../../components/input/Input';
+import Button from '../../components/button/Button';
 
 import './GuessTaking.css';
 
@@ -6,6 +8,20 @@ const GuessTaking = () => {
   return (
     <section className='GuessTaking__container'>
       <h1 className='GuessTaking__heading'>This is Guess Taking Page</h1>
+
+      <form>
+        <p data-testid='input-wrapper'>
+          <label>Player:</label>
+          <Input value='Your player' />
+        </p>
+
+        <Button
+          buttonSize='btn--medium'
+          buttonStyle='btn--gradient'
+        >
+          Submit Guess
+        </Button>
+      </form>
     </section>
   );
 };
