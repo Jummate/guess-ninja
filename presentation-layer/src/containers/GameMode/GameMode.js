9d652a94eb76e076dd2abc0cc3798c5b22eb4189
Jummate <yakubjumat@gmail.com>
2023-01-17 15:21:09 +0100

    feat: add buttons to the Menu component to specify the type of single or multi player game to play

--- presentation-layer/src/containers/GameMode/GameMode.js
+++ presentation-layer/src/containers/GameMode/GameMode.js
@@ -1,13 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./GameMode.css";
 import Button from "../../components/button/Button";
 import Navigation from "../../components/nav/Navigation";
 import { AppContext } from "../../utils/context";
 import Header from "../../components/header/Header";
+import Menu from "../../components/Menu";
 
 const GameMode = () => {
   const context = useContext(AppContext);
   const { contextDispatch } = context;
+  const [showMenu, setShowMenu] = useState(false);
+  const [mode, setMode] = useState("");
   return (
     <section className="GameMode__container">
       <Header
@@ -22,27 +25,160 @@ const GameMode = () => {
           <Button
             buttonSize="btn--large"
             buttonStyle="btn--gradient"
-            onClick={() =>
-              contextDispatch({
-                type: "SHOW_GAME_SETUP_PAGE",
-                payload: { selectedMode: "Single" },
-              })
-            }
+            onClick={(e) => (
+              setShowMenu(!showMenu), setMode(e.target.textContent.trim())
+            )}
+            // onClick={() =>
+            //   contextDispatch({
+            //     type: "SHOW_GAME_SETUP_PAGE",
+            //     payload: { selectedMode: "Single" },
+            //   })
+            // }
           >
             One Player
           </Button>
           <Button
             buttonSize="btn--large"
             buttonStyle="btn--gradient"
-            onClick={() =>
-              contextDispatch({
-                type: "SHOW_GAME_SETUP_PAGE",
-                payload: { selectedMode: "Multi" },
-              })
-            }
+            onClick={(e) => (
+              setShowMenu(!showMenu), setMode(e.target.textContent.trim())
+            )}
+            // onClick={() => (
+            //   contextDispatch({
+            //     type: "SHOW_GAME_SETUP_PAGE",
+            //     payload: { selectedMode: "Multi" },
+            //   }),
+            //   setMode("Multi")
+            // )}
           >
             Multiplayer
           </Button>
+
+          {showMenu ? (
+            <Menu>
+              {mode === "One Player" ? (
+                <>
+                  <Button
+                    buttonSize="btn--large"
+                    buttonStyle="btn--gradient"
+                    width="w-80"
+                    onClick={(e) =>
+                      contextDispatch({
+                        type: "SHOW_GAME_SETUP_PAGE",
+                        payload: {
+                          selectedMode: "Single",
+                          onePlayerGameType: `${e.target.textContent.trim()}`,
+                        },
+                      })
+                    }
+                  >
+                    PROGRESSIVE
+                  </Button>
+
+                  <Button
+                    buttonSize="btn--large"
+                    buttonStyle="btn--gradient"
+                    width="w-80"
+                    onClick={(e) =>
+                      contextDispatch({
+                        type: "SHOW_GAME_SETUP_PAGE",
+                        payload: {
+                          selectedMode: "Single",
+                          onePlayerGameType: `${e.target.textContent.trim()}`,
+                        },
+                      })
+                    }
+                  >
+                    CONSTANT
+                  </Button>
+
+                  <Button
+                    buttonSize="btn--large"
+                    buttonStyle="btn--gradient"
+                    width="w-80"
+                    onClick={(e) =>
+                      contextDispatch({
+                        type: "SHOW_GAME_SETUP_PAGE",
+                        payload: {
+                          selectedMode: "Single",
+                          onePlayerGameType: `${e.target.textContent.trim()}`,
+                        },
+                      })
+                    }
+                  >
+                    RANDOM
+                  </Button>
+                </>
+              ) : null}
+
+              {mode === "Multiplayer" ? (
+                <>
+                  <Button
+                    buttonSize="btn--large"
+                    buttonStyle="btn--gradient"
+                    width="w-80"
+                    onClick={(e) =>
+                      contextDispatch({
+                        type: "SHOW_GAME_SETUP_PAGE",
+                        payload: {
+                          selectedMode: "Multi",
+                          onePlayerGameType: `${e.target.textContent.trim()}`,
+                        },
+                      })
+                    }
+                  >
+                    ONE-TIME
+                  </Button>
+
+                  <Button
+                    buttonSize="btn--large"
+                    buttonStyle="btn--gradient"
+                    width="w-80"
+                    onClick={(e) =>
+                      contextDispatch({
+                        type: "SHOW_GAME_SETUP_PAGE",
+                        payload: {
+                          selectedMode: "Multi",
+                          onePlayerGameType: `${e.target.textContent.trim()}`,
+                        },
+                      })
+                    }
+                  >
+                    SESSION
+                  </Button>
+
+                  {/* <Button
+                    buttonSize="btn--large"
+                    buttonStyle="btn--gradient"
+                    width="w-80"
+                    onClick={(e) =>
+                      contextDispatch({
+                        type: "SHOW_GAME_SETUP_PAGE",
+                        payload: {
+                          selectedMode: "Multi",
+                          onePlayerGameType: `${e.target.textContent.trim()}`,
+                        },
+                      })
+                    }
+                  >
+                    RANDOM
+                  </Button> */}
+                </>
+              ) : null}
+
+              <Button
+                buttonSize="btn--large"
+                buttonStyle="btn--gradient"
+                width="w-80"
+                onClick={() => (
+                  contextDispatch({ type: "SHOW_HOME_PAGE" }),
+                  setShowMenu(!showMenu)
+                )}
+              >
+                BACK
+              </Button>
+            </Menu>
+          ) : null}
         </div>
       </div>
 
--- presentation-layer/src/utils/defaultState.js
+++ presentation-layer/src/utils/defaultState.js
@@ -1,4 +1,5 @@
 export const defaultState = {
+  onePlayerGameType: "",
   homePageActive: true,
   gameModeActive: false,
   gameSetupActive: false,
