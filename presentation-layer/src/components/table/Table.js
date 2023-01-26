8f7b746dc27b27dc7ff786a064d00eaeb8d213af
Jummate <yakubjumat@gmail.com>
2023-01-26 23:54:07 +0100

    chore(enhancement): complete the design of the score table
    feat: add close button to the score table

--- presentation-layer/src/components/table/Table.js
+++ presentation-layer/src/components/table/Table.js
@@ -1,32 +1,47 @@
 import { React } from "react";
+import Button from "../button/Button";
 import "./Table.css";
 
 const Table = ({ data = [], columns }) => {
   return (
-    <div className="table-wrapper">
-      <table className="table">
-        <thead>
-          <tr>
-            <td>S/N</td>
-            {columns &&
-              columns.map((head) => <th key={head.header}>{head.header}</th>)}
-          </tr>
-        </thead>
-        <tbody>
-          {data.length > 0 &&
-            data.map((row, index) => (
-              <tr key={row.ID}>
-                <td>{index + 1}</td>
-                {columns.map((col) => {
-                  return <td key={col.field}>{row[col.field]}</td>;
-                })}
-              </tr>
-            ))}
-        </tbody>
-      </table>
-      {!data.length && (
-        <p style={{ textAlign: "center" }}>No data to display</p>
-      )}
+    <div className="table-container">
+      <div className="table-wrapper">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>S/N</th>
+              {columns &&
+                columns.map((head) => <th key={head.header}>{head.header}</th>)}
+            </tr>
+          </thead>
+          <tbody>
+            {data.length > 0 &&
+              data.map((row, index) => (
+                <tr key={row.ID}>
+                  <td>{index + 1}</td>
+                  {columns.map((col) => {
+                    return <td key={col.field}>{row[col.field]}</td>;
+                  })}
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        {!data.length && (
+          <p style={{ textAlign: "center" }}>No data to display</p>
+        )}
+      </div>
+      <div className="table-button-wrapper">
+        <Button
+          buttonSize="btn--medium"
+          buttonStyle="btn--gradient"
+          width="w-60"
+          // onClick={() => (
+          //   contextDispatch({ type: "SHOW_HOME_PAGE" }), setShowMenu(!showMenu)
+          // )}
+        >
+          CLOSE
+        </Button>
+      </div>
     </div>
   );
 };
