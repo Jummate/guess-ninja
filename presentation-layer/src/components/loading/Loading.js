78976a5c7383132694e217e810d9c21742be6ca1
Jummate <yakubjumat@gmail.com>
2023-02-24 18:54:45 +0100

    chore: tidy up the loading page part of the UI

--- presentation-layer/src/components/loading/Loading.js
+++ presentation-layer/src/components/loading/Loading.js
@@ -3,7 +3,11 @@ import "./Loading.css";
 
 const Loading = ({ isSessionGame, sessionCount }) => (
   <div className="loading">
-    {isSessionGame ? <h1>SESSION {sessionCount}</h1> : null}
+    {isSessionGame ? (
+      <h1>
+        ROUND <span style={{ color: "#49cdcb" }}>{sessionCount}</span>
+      </h1>
+    ) : null}
     <h3>Loading...</h3>
   </div>
 );
