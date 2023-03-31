476d036f978a8c81d27f05ff9ea8ac8c11eb0636
Jummate <yakubjumat@gmail.com>
2023-03-31 19:23:37 +0100

    chore: implement the rendering and navigation of pages on Game Guide

--- presentation-layer/src/containers/GameGuide/GameGuide.js
+++ presentation-layer/src/containers/GameGuide/GameGuide.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 // import { AppContext } from "../../utils/context";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import Header from "../../components/header/Header";
 import Navigation from "../../components/nav/Navigation";
 import SoundIcon from "../../components/SoundController";
+import { contentSchema } from "../../utils/content-schema";
+import Content from "../../components/ContentRenderer";
 
 import "./GameGuide.css";
 
@@ -12,7 +14,9 @@ const GameGuide = () => {
 
   // const { contextDispatch } = context;
 
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const memoizedData = useMemo(() => contentSchema[count], [count]);
 
   return (
     <section className="GameGuide__container">
@@ -25,21 +29,37 @@ const GameGuide = () => {
 
       <div className="GameGuide__body">
         <div className="GameGuide__wrapper">
-          {count === 1 && <h2>YOU {count}</h2>}
-          {count === 2 && <h2>ARE {count}</h2>}
-          {count === 3 && <h2>WELCOME {count}</h2>}
-          {count === 4 && <h2>HERE {count}</h2>}
-          {count === 5 && <h2>BRO {count}</h2>}
+          <Content data={memoizedData} />
         </div>
       </div>
 
       <div className="GameGuide__footer">
         <div className="arrow-wrapper">
-          <FaChevronLeft onClick={() => setCount((prev) => prev - 1)} />
+          {count > 0 && (
+            <FaChevronLeft
+              onClick={() => {
+                if (count >= 1) {
+                  setCount((prev) => prev - 1);
+                }
+              }}
+            />
+          )}
+        </div>
+
+        <div>
+          {count + 1} of {contentSchema.length}
         </div>
 
         <div className="arrow-wrapper">
-          <FaChevronRight onClick={() => setCount((prev) => prev + 1)} />
+          {count < contentSchema.length - 1 && (
+            <FaChevronRight
+              onClick={() => {
+                if (count < contentSchema.length - 1) {
+                  setCount((prev) => prev + 1);
+                }
+              }}
+            />
+          )}
         </div>
       </div>
 
--- presentation-layer/src/utils/content-schema.js
+++ presentation-layer/src/utils/content-schema.js
@@ -1,11 +1,61 @@
 export const contentSchema = [
   {
-    mainHeading: "This is main heading",
-    mainContent: "This is main content",
-    subHeading: "This is sub-heading",
+    mainHeading: "Mode Selection",
+    mainContent:
+      "On the Game Mode Page, click any of the buttons to indicate the desired mode",
     list: [
-      { listTitle: "This is List Title", listContent: "This is List Content" },
-      { listTitle: "Another List Title", listContent: "Another List Content" },
+      {
+        heading: "",
+        item: [
+          { title: "Single Player:", content: "involves one player" },
+          {
+            title: "Multiplayer:",
+            content: "involves more than one player",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    mainHeading: `Mode Type`,
+    mainContent: `On the next page that shows up after specifying the desired mode,
+    click any of the buttons to specify the desired Mode Type`,
+
+    list: [
+      {
+        heading: "For Single Player:",
+        item: [
+          {
+            title: "Progressive:",
+            content: "game difficulty increases as game progresses",
+          },
+          {
+            title: "Constant:",
+            content: "difficulty selected is maintained throughout",
+          },
+          {
+            title: "Random:",
+            content: "difficulty varies as game progresses",
+          },
+        ],
+      },
+
+      {
+        heading: "For Multiplayer:",
+        item: [
+          {
+            title: "Regular:",
+            content:
+              "similar to Constant modetype but it involves multiple players",
+          },
+          {
+            title: "Session:",
+            content:
+              "number of rounds to go is selected and the winner is declared after the specified number of rounds has been completed",
+          },
+        ],
+      },
     ],
   },
 ];
